3240f7eebe274de74a249adecc44556755bbee03
zukash <48586209+zukash@users.noreply.github.com>
2024-11-12 01:01:06 +0900

    Add crypto/lll

--- ctftools/crypto/lll.py
+++ ctftools/crypto/lll.py
@@ -3,23 +3,25 @@ from sage.all import *
 
 def subset_sum_problem(S, t, verbose=False):
     """
-    # ref. https://kmyk.github.io/blog/writeups/ctf-2015-plaidctf-2015-lazy/
-    # ref. https://qiita.com/kusano_k/items/5509bff6e426e5043591
+    ref. https://kmyk.github.io/blog/writeups/ctf-2015-plaidctf-2015-lazy/
+    ref. https://qiita.com/kusano_k/items/5509bff6e426e5043591
     """
     n = len(S)
-    M = [[0] * (n + 1) for _ in range(n + 1)]
+    XX = [[0] * (n + 1) for _ in range(n + 1)]
     for i in range(n):
-        M[i][i] = 2
-        M[i][-1] = S[i] * n
-        M[-1][i] = 1
-    M[-1][-1] = t * n
+        XX[i][i] = 2
+        XX[i][-1] = S[i] * n
+        XX[-1][i] = 1
+    XX[-1][-1] = t * n
 
-    res = matrix(M).LLL()
+    YY = matrix(XX).LLL()
 
     if verbose:
-        return res
+        return YY
     else:
-        res = res[0][:-1]
-        assert all(x in [-1, 1] for x in res)
-        # -1 -> 1, 1 -> 0
-        return [(1 - x) // 2 for x in res]
+        for Y in YY:
+            Y = Y[:-1]
+            if all(y in [-1, 1] for y in Y):
+                # -1 -> 1, 1 -> 0
+                return [(1 - x) // 2 for x in Y]
+        assert False, "no solution"
